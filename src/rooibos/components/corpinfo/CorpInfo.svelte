28f8369ed00e2d10fd62668de85f2f614467570d
toublence <toublence.nam@gmail.com>
2025-01-24 09:47:24 +0900

    .

--- src/rooibos/components/corpinfo/CorpInfo.svelte
+++ src/rooibos/components/corpinfo/CorpInfo.svelte
@@ -298,20 +298,19 @@
 						</svg>
 					</button>
 				{:else}
-				<button 
-					class="p-2 hover:bg-gray-100 rounded-full" 
-					on:click={isFullscreen ? toggleFullscreen : toggleFullscreen}
-				>
-			  
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-				  <path 
-					stroke-linecap="round" 
-					stroke-linejoin="round" 
-					stroke-width="2" 
-					d={isFullscreen ? "M19 9l-7 7-7-7" : "M5 15l7-7 7 7"}
-				  />
-				</svg>
-			  </button>
+					<button 
+						class="p-2 hover:bg-gray-100 rounded-full" 
+						on:click={isFullscreen ? closeCompanyInfo : toggleFullscreen}
+					>
+				
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path 
+							stroke-linecap="round" 
+							stroke-linejoin="round" 
+							stroke-width="2" 
+							d={isFullscreen ? "M6 18L18 6M6 6l12 12" : "M5 15l7-7 7 7"}
+						   />
+					</button>
 				
 				{/if}
 			</div>
