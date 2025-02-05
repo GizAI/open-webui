9c237e07c9e75f260a29e4ee044e524807727c28
toublence <toublence.nam@gmail.com>
2025-02-05 11:49:45 +0900

    .

--- src/rooibos/components/corpbookmarks/CorpBookmarksBase.svelte
+++ src/rooibos/components/corpbookmarks/CorpBookmarksBase.svelte
@@ -801,29 +801,29 @@
 						</span>
 					</button>
 
-				
 					<!-- AI 채팅 버튼 -->
-					<button class="flex flex-col items-center hover:bg-gray-100 rounded-lg" on:click={() => openAIChat(bookmark)}>
+					<button class="flex flex-col items-center hover:bg-gray-100 rounded-lg" on:click={() => openAIChat(companyInfo)}>
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h8m-8 4h5m-9 6v-6a2 2 0 012-2h12a2 2 0 012 2v6l-4-4H5l-4 4z" />
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
 						</svg>
 						<span class="text-xs text-gray-500 mt-1 whitespace-nowrap">AI채팅</span>
 					</button>
-				
-					<!-- 네이버 찾기 버튼 -->
-					<button class="flex flex-col items-center hover:bg-gray-100 rounded-lg" on:click={() => searchNaver(bookmark)}>
+						
+						<!-- 네이버 지도 버튼 -->
+					<button class="flex flex-col items-center hover:bg-gray-100 rounded-lg" on:click={() => searchNaver(companyInfo)}>
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m1.5-4.15a7 7 0 11-14 0 7 7 0 0114 0z" />
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
 						</svg>
-						<span class="text-xs text-gray-500 mt-1 whitespace-nowrap">네이버</span>
+						<span class="text-xs text-gray-500 mt-1 whitespace-nowrap">네이버지도</span>
 					</button>
 				
 					
-						<button class="hover:bg-gray-100 rounded-full" on:click={closeCompanyInfo}>
-							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-							</svg>
-						</button>
+					<button class="hover:bg-gray-100 rounded-full" on:click={closeCompanyInfo}>
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
 					
 				</div>
 			</div>
