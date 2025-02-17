39b5d7130f53ac0abd7ebbad5ad415c4f25b0be7
toublence <toublence.nam@gmail.com>
2025-02-17 16:07:17 +0900

    .

--- src/rooibos/components/corpbookmarks/CorpBookmarksBase.svelte
+++ src/rooibos/components/corpbookmarks/CorpBookmarksBase.svelte
@@ -710,7 +710,7 @@
 				</div>
 
 				<!-- attach file -->
-				<div class="flex flex-row flex-1 h-1/2 max-h-1/2 pb-2.5 gap-3">
+				<div class="flex flex-row pb-2.5 gap-3">
 					{#if largeScreen}
 						<div class="flex-1 flex justify-start w-full h-full max-h-full">
 							{#if selectedFile}
