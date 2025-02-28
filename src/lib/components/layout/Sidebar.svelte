2eb41a5629ad18f89b95ffd59c2a938394fba0a9
toublence <toublence.nam@gmail.com>
2025-02-28 16:47:01 +0900

    .

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -538,7 +538,13 @@
 				</a>
 			</div>
 			<div class="px-1.5 flex justify-center text-gray-800 dark:text-gray-200">
-				
+				{#if $user?.id != '5ee82147-b469-4609-8bea-2ba484cfc2ab'}
+				<div class="self-center">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-[1.1rem]">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345l2.125-5.111z"/>
+					</svg>
+				</div>
+				{/if}
 					<div class="flex self-center w-full">
 						{#if $user?.id == '5ee82147-b469-4609-8bea-2ba484cfc2ab'}
 							<NoteFolder
