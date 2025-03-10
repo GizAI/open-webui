674b56bda328ea603d822e5e90039df6bad2afb2
toublence <toublence.nam@gmail.com>
2025-03-10 16:02:18 +0900

    .

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -606,59 +606,24 @@
 			</div>
 			<div class="px-1.5 flex justify-center text-gray-800 dark:text-gray-200">
 				<div class="flex self-center w-full">
-					{#if $user?.id == '5ee82147-b469-4609-8bea-2ba484cfc2ab'}
-						<RooibosFolder
-							collapsible={!search}
-							className="w-full flex justify-between items-center"
-							name="나의 기업"
-							onAdd={() => createRooibosFolder('Untitled', 'corp')}
-							onAddLabel={$i18n.t('New Folder')}
-							open={false}
-						>
-							<RooibosFolderMenu
-								folders={Object.values(rooibosFolders)
-									.filter((folder) => folder.type === 'corp')
-									.reduce((acc, folder) => {
-										acc[folder.id] = folder;
-										return acc;
-									}, {})}
-								parentId={null}
-							/>
-						</RooibosFolder>
-					{:else}
-						<a
-							class="flex-grow flex space-x-3 rounded-lg px-2 py-[7px] hover:bg-gray-100 dark:hover:bg-gray-900 transition"
-							href="/rooibos/corpbookmarks"
-							on:click={() => {
-								selectedChatId = null;
-								chatId.set('');
-								if ($mobile) {
-									showSidebar.set(false);
-								}
-							}}
-							draggable="false"
-						>
-							{#if $user?.id != '5ee82147-b469-4609-8bea-2ba484cfc2ab'}
-								<div class="self-center">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="2"
-										stroke="currentColor"
-										class="size-[1.1rem]"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345l2.125-5.111z"
-										/>
-									</svg>
-								</div>
-							{/if}
-							<div class="self-center font-medium text-sm font-primary">나의 기업</div>
-						</a>
-					{/if}
+					<RooibosFolder
+						collapsible={!search}
+						className="w-full flex justify-between items-center"
+						name="나의 기업"
+						onAdd={() => createRooibosFolder('Untitled', 'corp')}
+						onAddLabel={$i18n.t('New Folder')}
+						open={false}
+					>
+						<RooibosFolderMenu
+							folders={Object.values(rooibosFolders)
+								.filter((folder) => folder.type === 'corp')
+								.reduce((acc, folder) => {
+									acc[folder.id] = folder;
+									return acc;
+								}, {})}
+							parentId={null}
+						/>
+					</RooibosFolder>
 				</div>
 			</div>
 			
