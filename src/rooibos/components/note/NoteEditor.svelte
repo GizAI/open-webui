fb1f473a232239db645275633525922d67cf7795
toublence <toublence.nam@gmail.com>
2025-03-12 14:56:25 +0900

    .

--- src/rooibos/components/note/NoteEditor.svelte
+++ src/rooibos/components/note/NoteEditor.svelte
@@ -524,7 +524,7 @@
 		const providerInstance = new HocuspocusProvider({
 			url: window.location.hostname === 'localhost'
 				? 'ws://localhost:8443'
-				: 'wss://hocuspocus.conting.ai:8443',
+				: 'wss://hocuspocus.conting.ai/ws',
 			name: documentName,
 			token: token,
 			connect: true,
