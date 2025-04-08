350a024f2f31d81db916d0f25ef3bbe29b74b5b8
toublence <toublence.nam@gmail.com>
2025-04-08 15:09:01 +0900

    .

--- src/rooibos/components/company/PrivateCompanyDetail.svelte
+++ src/rooibos/components/company/PrivateCompanyDetail.svelte
@@ -274,7 +274,7 @@
 						
 						<div class="flex justify-end space-x-2 mt-4 p-2">
 							<button
-								class="px-4 py-2 text-sm font-medium text-blue-600 border border-blue-600 hover:bg-blue-50 bg-transparent rounded-md transition-colors"
+								class="px-4 py-2 text-sm font-medium text-yellow-500 border border-yellow-500 hover:bg-blue-50 bg-transparent rounded-md transition-colors"
 								on:click={saveData}
 								disabled={isSaving}
 							>
