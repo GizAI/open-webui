08de3d9e71567dc85782980d7f2cced829a7b0d2
toublence <toublence.nam@gmail.com>
2025-04-09 14:14:01 +0900

    .

--- src/rooibos/components/mycompany/MyCompanyInfo.svelte
+++ src/rooibos/components/mycompany/MyCompanyInfo.svelte
@@ -1363,7 +1363,7 @@
 							<h2 class="text-sm font-medium flex items-center">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
-									class="h-4 w-4 mr-1 text-gray-500"
+									class="h-4 w-4 mr-1 text-amber-500 dark:text-amber-400"
 									fill="none"
 									viewBox="0 0 24 24"
 									stroke="currentColor"
@@ -1515,7 +1515,7 @@
 							<h2 class="text-sm font-medium flex items-center">
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
-									class="h-4 w-4 mr-1 text-gray-500"
+									class="h-4 w-4 mr-1 text-gray-600 dark:text-gray-300"
 									fill="none"
 									viewBox="0 0 24 24"
 									stroke="currentColor"
@@ -1668,16 +1668,16 @@
 								<h2 class="text-sm font-medium flex items-center">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
-										class="h-4 w-4 mr-1 text-gray-500"
+										class="h-4 w-4 mr-1 text-teal-500 dark:text-teal-400"
 										fill="none"
 										viewBox="0 0 24 24"
 										stroke="currentColor"
 									>
 										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
 										/>
 									</svg>
 									채팅
